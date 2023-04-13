1d4dbe39890beeb77049673ce89256f3a4b9774f
norris2304 <104096036+norris2304@users.noreply.github.com>
2023-04-13 15:56:25 -0500

    Fixed grid not resizing correctly

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const grid = document.querySelector(".gridContainer");
 const userInput = document.getElementById("quantity");
 const resetButton = document.querySelector(".reset");
+let squaresPerSide = 16;
 
 //Creates the entire grid
 createGrid = () => {
-    for (let i = 0; i < 256; i++) {
+    let numberOfCells = squaresPerSide * squaresPerSide;
+    grid.style.setProperty("grid-template-columns", `repeat(${squaresPerSide}, 1fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(${squaresPerSide}, 1fr)`);
+    for (let i = 0; i < numberOfCells; i++) {
         const div = document.createElement("div");
         div.classList.add("square");
         grid.appendChild(div);
@@ -14,14 +18,8 @@ createGrid = () => {
 //Allows user to choose grid size
 updateGrid = () =>  {
     grid.innerHTML = "";
-    grid.style.setProperty(
-        "grid-template-columns",
-        `repeat(${userInput.value}, 1fr)`
-    );
-    grid.style.setProperty(
-        "grid.template-rows",
-        `repeat(${userInput.value}, 1fr)`
-    );
+    grid.style.setProperty("grid-template-columns", `repeat(${userInput.value}, 1fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(${userInput.value}, 1fr)`);
     for (let i = 0; i < (userInput.value * userInput.value); i++) {
         const div = document.createElement("div");
         div.classList.add("square");
@@ -40,8 +38,8 @@ userInput.addEventListener("change", updateGrid);
 resetButton.addEventListener("click", function() {
     grid.innerHTML = "";
     userInput.value = "";
-    grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
-    grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+    grid.style.setProperty("grid-template-columns", `repeat(16, 1fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(16, 1fr)`);
     createGrid();
 });
 
